6db51ff3771fd954d776e7d30d1f90362c6c3543
Yammili <119533369+Yammili@users.noreply.github.com>
2023-01-10 13:23:49 +0200

    Update bank_sql_mimesis.py

--- bank_sql_mimesis.py
+++ bank_sql_mimesis.py
@@ -20,22 +20,23 @@ class People(Base):
     def __init__(self, **kwargs):
         super(People, self).__init__(**kwargs)
 
-    def _bootstrap(count, locale: str):
-        generic = Generic(locale)
-        
-        for _ in range(count):
-            people = People(
-                id = generic.numeric.increment(),
-                full_name = generic.person.full_name(),
-                nationality = generic.person.nationality(),
-                age = generic.person.age(minimum=16, maximum=100),
-                job = generic.person.occupation(),
-                phone = generic.person.telephone(),
-                email = generic.person.email()
-            )
-            session.add(people)
-            session.commit()
-            print(people)
+    def bootstrap(count, locale: str):
+        if 0 <= count <= 5000:
+            generic = Generic(locale)
+            
+            for _ in range(count):
+                people = People(
+                    id = generic.numeric.increment(),
+                    full_name = generic.person.full_name(),
+                    nationality = generic.person.nationality(),
+                    age = generic.person.age(minimum=16, maximum=100),
+                    job = generic.person.occupation(),
+                    phone = generic.person.telephone(),
+                    email = generic.person.email()
+                )
+                session.add(people)
+                session.commit()
+        else: print("The number of instances of model 'People' is not in the range: 0 : 5000")
 
 
 class Card(Base):
@@ -51,21 +52,22 @@ class Card(Base):
     def __init__(self, **kwargs):
         super(Card, self).__init__(**kwargs)
 
-    def _bootstrap(count, locale: str):
-        generic = Generic(locale)
-        
-        for _ in range(count):
-            card = Card(
-                id = generic.numeric.increment(),
-                number = generic.payment.credit_card_number(),
-                data = generic.payment.credit_card_expiration_date(minimum=16, maximum=27),
-                cvv = generic.payment.cvv(),
-                validity = generic.random.randint(a=4, b=5),
-                owner = generic.person.full_name()
-            ) 
-            session.add(card)
-            session.commit()
-            print(card)
+    def bootstrap(count, locale: str):
+        if 0 <= count <= 5000:
+            generic = Generic(locale)
+            
+            for _ in range(count):
+                card = Card(
+                    id = generic.numeric.increment(),
+                    number = generic.payment.credit_card_number(),
+                    data = generic.payment.credit_card_expiration_date(minimum=16, maximum=27),
+                    cvv = generic.payment.cvv(),
+                    validity = generic.random.randint(a=4, b=5),
+                    owner = generic.person.full_name()
+                ) 
+                session.add(card)
+                session.commit()
+        else: print("The number of instances of model 'Card' is not in the range: 0 : 5000")
 
 
 class Address(Base):
@@ -82,38 +84,43 @@ class Address(Base):
     def __init__(self, **kwargs):
         super(Address, self).__init__(**kwargs)
 
-    def _bootstrap(count, locale: str):
-        generic = Generic(locale)
-        
-        for _ in range(count):
-            address = Address(
-                id = generic.numeric.increment(),
-                country = generic.address.country(),
-                city = generic.address.city(),
-                street = generic.address.street_name(),
-                house = generic.address.street_number(maximum=400),
-                zip_code = generic.address.zip_code(),
-                inhabitant = generic.person.full_name()
-            )
-            session.add(address)
-            session.commit()
-            print(address)
+    def bootstrap(count, locale: str):
+        if 0 <= count <= 5000:   
+            generic = Generic(locale)
+            
+            for _ in range(count):
+                address = Address(
+                    id = generic.numeric.increment(),
+                    country = generic.address.country(),
+                    city = generic.address.city(),
+                    street = generic.address.street_name(),
+                    house = generic.address.street_number(maximum=400),
+                    zip_code = generic.address.zip_code(),
+                    inhabitant = generic.person.full_name()
+                )
+                session.add(address)
+                session.commit()
+        else: print("The number of instances of model 'Address' is not in the range: 0 : 5000")
+
+def pars():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("it_people", type=int, help="Number of iterations for model 'People'")
+    parser.add_argument("it_card", type=int, help="Number of iterations for model 'Card'")
+    parser.add_argument("it_address", type=int, help="Number of iterations for model 'Address'")
+
+    args = parser.parse_args()
+    
+    People.bootstrap(args.it_people,"en")
+    Card.bootstrap(args.it_card,"en")
+    Address.bootstrap(args.it_address,"en")
 
 
-engine = db.create_engine("sqlite:///base.db", echo=True)
-Base.metadata.create_all(bind=engine)
+if __name__ == "__main__":
+    engine = db.create_engine("sqlite:///base.db", echo=True)
+    Base.metadata.create_all(bind=engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("it_people", type=int, help="Number of iterations for model 'People'")
-parser.add_argument("it_card", type=int, help="Number of iterations for model 'Card'")
-parser.add_argument("it_address", type=int, help="Number of iterations for model 'Address'")
-
-args = parser.parse_args()
-
-People._bootstrap(args.it_people,"en")
-Card._bootstrap(args.it_card,"en")
-Address._bootstrap(args.it_address,"en")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    pars()
